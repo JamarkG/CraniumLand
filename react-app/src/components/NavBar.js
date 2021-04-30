4180e7342d3f11295ebae80fa4b3e6dc4a08522a
Mark Gregory <markgregory19@gmail.com>
2021-04-30 08:38:09 -0700

    started styling navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
 import {login} from "../store/session"
 import LogoutButton from './auth/LogoutButton';
+import './CSS/NavBar.css';
 
 const NavBar = () => {
   const dispatch = useDispatch();
@@ -20,42 +21,43 @@ const NavBar = () => {
 
   return (
     <nav>
-      <ul>
-        <li>
+      <div className='TopHalfNav'>
+        <div className='NavButton' id='HomeLink'>
           <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div hidden={user} className='NavButton' id='LoginButton'>
           <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
-        </li>
-        <li  hidden={user}>
+        </div>
+        <div hidden={user} className='NavButton' id='DemoLoginButton'>
           <button onClick={async (e)=>{await dispatch(login('demo@aa.io', 'password'))}}>
             Demo Log In
           </button>
-        </li>
-        <li  hidden={user}>
+        </div>
+        <div hidden={user} className='NavButton' id='SignUpButton'>
           <NavLink to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div className='NavButton' id='DeckListButton'>
           <NavLink to="/decks" exact={true} activeClassName="active">
             Decks
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div hidden={!user}>
           <LogoutButton />
-        </li>
-      </ul>
+        </div>
+      </div>
       <form>
         <input
+        id='searchBar'
         type='text'
         placeholder='Search by Deck Name or Tags'
         onChange={(e) => setSearchTerm(e.target.value)}></input>
-        <button onClick={searchGo}>Search</button>
+        <button id='searchButton' onClick={searchGo}>Search</button>
       </form>
     </nav>
   );
