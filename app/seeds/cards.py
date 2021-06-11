1c4d1e0d5494d89f5d15ebfc906cf893f3696bfa
Caleb <cpbeachler@gmail.com>
2021-06-11 10:52:42 -0400

    updated seeded cards

--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -2,9 +2,9 @@ from app.models import db, Card
 
 def seed_cards():
 
-    card1 = Card(deckid='1', answer='Yes', question='Did this work?')
-    card2 = Card(deckid='1', answer='2nd card', question='What card is this?')
-    card3= Card(deckid='2', answer='In the 2nd deck', question='What deck is this in?')
+    card1 = Card(deckid='1', answer='What is the syntax of a For Loop?', question='for(let i = 0; i < x; i++)')
+    card2 = Card(deckid='1', answer='What is .length()?', question='Method for iterables to determine number of elements.')
+    card3= Card(deckid='2', answer='What is an array?', question='Ordered list of values.')
 
     db.session.add(card1)
     db.session.add(card2)
